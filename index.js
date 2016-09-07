75e4c78a8f929f3854417d78c854f0533cdf3017
Bell App Lab <apps@bellapplab.com>
2016-09-07 12:58:28 +0100

    There there

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(function (req, res, next) {
-  res.header("X-Powered-By", "Bells and whistles");
-  next();
-});
-
 app.get('/adrock', function (req, res) {
   res.send('Hello World!');
 });

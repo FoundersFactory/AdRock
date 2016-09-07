035e3c42573b961c2093e2cc9a483e41aadc8a1c
Bell App Lab <apps@bellapplab.com>
2016-09-07 12:53:32 +0100

    Trying to hide powered by

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/adrock', function (req, res) {
-  res.send('Hello World!');
-});
-
 app.use(function (req, res, next) {
   res.header("X-Powered-By", "Bells and whistles");
   next();
+});
+
+app.get('/adrock', function (req, res) {
+  res.send('Hello World!');
 });
 
 app.listen(3000, 'localhost', function () {

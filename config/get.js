e0ab58ede20cf79bf6a57f5fcc7135cee10b39e0
Bell App Lab <apps@bellapplab.com>
2016-09-09 10:55:23 +0100

    Implemented GET and POST \o/

--- config/get.js
+++ config/get.js
@@ -1,34 +1,27 @@
 const fs = require("fs");
+const sanitiser = require("./sanitiser.js");
 
 //Getting apps
 module.exports = function(app) {
-	app.get("/adrock", function (req, res) 
+	app.get("/adrock", function(req, res) 
 	{
-		function error() {
+		var error = function() {
 			res.sendStatus(404);
 		}
 		
-		var path = null;
-		try {
-			path = req.originalUrl.toLowerCase().split("/")[0];
-		} catch (e) {
-			error();
-		}
-		
-		if (path == null) {
-			error();
-			return;
-		}
-		
-		path = "./apps/" + path + "/index.html";
-		
-		fs.stat(path, function(err, stats) {
-			if (err == null || err == undefined) {
-				res.render(path);
-				return;
-			}
+		sanitiser.sanitise(req, function(path) {
+			if (path == null) path = "index.html";
 			
-			error();
+			path = sanitiser.rootPath + "/" + path;
+			
+			fs.stat(path, function(err, stats) {
+				if (err == null || err == undefined) {
+					res.status(200).send(path);
+					return;
+				}
+				
+				error();
+			});
 		});
 	});
 };
---
+++ config/sanitiser.js
@@ -0,0 +1,25 @@
+module.exports = {
+	post: function(req, callback) {
+		if (req.path == null || req.path.length == 0) {
+			return "";
+		}
+		return null;
+	},
+	get: function(req, callback) {
+		var result = null;
+		
+		if (req.query.length > 0) {
+			callback(result);
+			return;
+		}
+		
+		try {
+			result = req.path.toLowerCase();
+		} catch (e) {
+			//Noop
+		} finally {
+			callback(result);
+		}
+	},
+	rootPath: "./apps"
+};
